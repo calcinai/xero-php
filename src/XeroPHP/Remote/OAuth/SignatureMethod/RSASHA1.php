bb23ade33a91158d9abed4b294e35d60a2e4d964
Gerard (Gerry) Caulfield <git@caulfield.me>
2015-06-29 15:56:11 +1000

    Make inaccessable private key exception more informative
    Perviously setting $config['rsa_private_key'] = 'file://full/path/to/key.pem' (note two slashes instead of three) or other variations that are incompatitible with openssl_pkey_private(), would just give the user "Cannot access private key for signing", for a file that is very much accessable. The additional info I've added allows the user to go straight to the manual instead of spending time thinking it's a permissions issue.

--- src/XeroPHP/Remote/OAuth/SignatureMethod/RSASHA1.php
+++ src/XeroPHP/Remote/OAuth/SignatureMethod/RSASHA1.php
@@ -9,8 +9,9 @@ class RSASHA1 implements SignatureMethodInterface {
 
     public static function generateSignature(array $config, $sbs, $secret) {
         // Fetch the private key
-        if(false === $private_key_id = openssl_pkey_get_private($config['rsa_private_key']))
-            throw new Exception('Cannot access private key for signing');
+        if(false === $private_key_id = openssl_pkey_get_private($config['rsa_private_key'])){
+            throw new Exception("Cannot access private key for signing: openssl_pkey_get_private('${config['rsa_private_key']}') returned false");
+        }
 
         // Sign using the key
         if(false === openssl_sign($sbs, $signature, $private_key_id)) {
