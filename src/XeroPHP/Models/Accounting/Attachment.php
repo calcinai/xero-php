d91a8867d855ae9fda657a6219989827adee9b63
Amo Chohan <amo.chohan@gmail.com>
2015-08-14 09:14:03 +0000

    Add isPageable method

--- src/XeroPHP/Models/Accounting/Attachment.php
+++ src/XeroPHP/Models/Accounting/Attachment.php
@@ -201,4 +201,11 @@ class Attachment extends Object {
         $this->local_handle = $local_handle;
     }
 
+    /**
+     * @return mixed
+     */
+    static function isPageable() {
+        return false;
+    }
+
 }
