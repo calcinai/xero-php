calcinai/xero-php

commit cc19588688ba23353c89a54e1cff4fc16ec0f445

Fixes #88 - oversight when implementing Collections

src/XeroPHP/Remote/Object.php
@@ -208,7 +208,7 @@ abstract class Object implements ObjectInterface, \JsonSerializable, \ArrayAcces
 
             $type = $meta[self::KEY_TYPE];
 
-            if(is_array($this->_data[$property])) {
+            if($this->_data[$property] instanceof Collection) {
                 $out[$property] = array();
                 foreach($this->_data[$property] as $assoc_property) {
                     $out[$property][] = self::castToString($type, $assoc_property);
@@ -324,7 +324,7 @@ abstract class Object implements ObjectInterface, \JsonSerializable, \ArrayAcces
                         $obj = $this->_data[$property];
                         $obj->validate();
 
-                    } elseif(is_array($this->_data[$property])) {
+                    } elseif($this->_data[$property] instanceof Collection) {
                         foreach($this->_data[$property] as $element) {
                             if($element instanceof Object)
                                 $element->validate();

commit 9ebfcaf540dee424b666c6694af349e489c7a0a7

Fixes what cc19588 broke in #88, creates empty collections when model is created

src/XeroPHP/Remote/Object.php
@@ -71,6 +71,12 @@ abstract class Object implements ObjectInterface, \JsonSerializable, \ArrayAcces
         $this->_dirty = array();
         $this->_data = array();
         $this->_associated_objects = array();
+
+        foreach(static::getProperties() as $property => $data){
+            if($data[self::KEY_IS_ARRAY]){
+                $this->_data[$property] = new Collection();
+            }
+        }
     }
 
     /**