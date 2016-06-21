a63055c62bbb10e2bff97e041877c5db6c1ea914
max.snow <max.snow@4mation.com.au>
2016-06-21 20:36:55 +1000

    Add Balance Sheet Report

---
+++ src/XeroPHP/Models/Accounting/Report/BalanceSheet.php
@@ -0,0 +1,261 @@
+<?php
+namespace XeroPHP\Models\Accounting\Report;
+
+use XeroPHP\Remote;
+
+class BalanceSheet extends Remote\Object
+{
+
+    /**
+     * Xero identifier
+     *
+     * @property string ReportID
+     */
+
+    /**
+     * Report Name
+     *
+     * @property string ReportName
+     */
+
+    /**
+     * Report Type
+     *
+     * @property string ReportType
+     */
+
+    /**
+     * Array of the title of this report (one title will be on multiple lines)
+     *
+     * @property array ReportTitles
+     */
+
+    /**
+     * Last modified date in UTC format
+     *
+     * @property \DateTimeInterface UpdatedDateUTC
+     */
+
+    /**
+     * All the rows of data contained in the report
+     *
+     * @property string Rows
+     */
+
+
+    /**
+     * Get the resource uri of the class (Contacts) etc
+     *
+     * @return string
+     */
+    public static function getResourceURI()
+    {
+        return 'Reports/BalanceSheet';
+    }
+
+
+    /**
+     * Get the root node name.  Just the unqualified classname
+     *
+     * @return string
+     */
+    public static function getRootNodeName()
+    {
+        return 'Report';
+    }
+
+
+    /**
+     * Get the guid property
+     *
+     * @return string
+     */
+    public static function getGUIDProperty()
+    {
+        return 'Id';
+    }
+
+
+    /**
+     * Get the stem of the API (core.xro) etc
+     *
+     * @return string|null
+     */
+    public static function getAPIStem()
+    {
+        return Remote\URL::API_CORE;
+    }
+
+
+    /**
+     * Get the supported methods
+     */
+    public static function getSupportedMethods()
+    {
+        return array(
+            Remote\Request::METHOD_GET,
+        );
+    }
+
+    /**
+     *
+     * Get the properties of the object.  Indexed by constants
+     *  [0] - Mandatory
+     *  [1] - Type
+     *  [2] - PHP type
+     *  [3] - Is an Array
+     *  [4] - Saves directly
+     *
+     * @return array
+     */
+    public static function getProperties()
+    {
+        return array(
+            'ReportID' => array (true, self::PROPERTY_TYPE_STRING, null, false, false),
+            'ReportName' => array (true, self::PROPERTY_TYPE_STRING, null, false, false),
+            'ReportType' => array (true, self::PROPERTY_TYPE_STRING, null, false, false),
+            'ReportTitles' => array (true, self::PROPERTY_TYPE_STRING, null, true, false),
+            'ReportDate' => array (true, self::PROPERTY_TYPE_DATE, null, false, false),
+            'UpdatedDateUTC' => array (true, self::PROPERTY_TYPE_TIMESTAMP, '\\DateTimeInterface', false, false),
+            'Rows' => array (true, self::PROPERTY_TYPE_STRING, null, true, false),
+        );
+    }
+
+    public static function isPageable()
+    {
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReportID()
+    {
+        return $this->_data['ReportID'];
+    }
+
+    /**
+     * @param string $value
+     * @return BalanceSheet
+     */
+    public function setReportID($value)
+    {
+        $this->propertyUpdated('ReportID', $value);
+        $this->_data['ReportID'] = $value;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReportName()
+    {
+        return $this->_data['ReportName'];
+    }
+
+    /**
+     * @param string $value
+     * @return BalanceSheet
+     */
+    public function setReportName($value)
+    {
+        $this->propertyUpdated('ReportName', $value);
+        $this->_data['ReportName'] = $value;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReportType()
+    {
+        return $this->_data['ReportType'];
+    }
+
+    /**
+     * @param string $value
+     * @return BalanceSheet
+     */
+    public function setReportType($value)
+    {
+        $this->propertyUpdated('ReportType', $value);
+        $this->_data['ReportType'] = $value;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReportTitles()
+    {
+        return $this->_data['ReportTitles'];
+    }
+
+    /**
+     * @param string $value
+     * @return BalanceSheet
+     */
+    public function setReportTitles($value)
+    {
+        $this->propertyUpdated('ReportTitles', $value);
+        $this->_data['ReportTitles'] = $value;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReportDate()
+    {
+        return $this->_data['ReportDate'];
+    }
+
+    /**
+     * @param string $value
+     * @return BalanceSheet
+     */
+    public function setReportDate($value)
+    {
+        $this->propertyUpdated('ReportDate', $value);
+        $this->_data['ReportDate'] = $value;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUpdatedDateUTC()
+    {
+        return $this->_data['UpdatedDateUTC'];
+    }
+
+    /**
+     * @param string $value
+     * @return BalanceSheet
+     */
+    public function setUpdatedDateUTC($value)
+    {
+        $this->propertyUpdated('UpdatedDateUTC', $value);
+        $this->_data['UpdatedDateUTC'] = $value;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRows()
+    {
+        return $this->_data['Rows'];
+    }
+
+    /**
+     * @param string $value
+     * @return BalanceSheet
+     */
+    public function setRows($value)
+    {
+        $this->propertyUpdated('Rows', $value);
+        $this->_data['Rows'] = $value;
+        return $this;
+    }
+}
