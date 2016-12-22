c1c43777ba3a270a36da8e76df1b11ffaf834605
SidneyAllen <sid.maestre@gmail.com>
2016-12-22 10:32:34 -0800

    Removed config defaults from Partner App
    Redundant as are already defined correctly in the Application this
    class extends

--- src/XeroPHP/Application/PartnerApplication.php
+++ src/XeroPHP/Application/PartnerApplication.php
@@ -6,10 +6,5 @@ use XeroPHP\Application;
 
 class PartnerApplication extends Application
 {
-    protected static $_type_config_defaults = [
-        'xero' => [
-            'site'     => 'https://api.xero.com',
-            'base_url' => 'https://api.xero.com',
-        ]
-    ];
+    protected static $_type_config_defaults = [];
 }
