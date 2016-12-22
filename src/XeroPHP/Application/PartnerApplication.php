3675bd0ac70104482ab53047db2bea6810f3e1ec
SidneyAllen <sid.maestre@gmail.com>
2016-12-22 09:54:47 -0800

    Removed entrust ssl certificates
    In 2017, Xero is deprecating the entrust ssl certificate requirement
    for partner apps

--- src/XeroPHP/Application/PartnerApplication.php
+++ src/XeroPHP/Application/PartnerApplication.php
@@ -8,8 +8,8 @@ class PartnerApplication extends Application
 {
     protected static $_type_config_defaults = [
         'xero' => [
-            'site'     => 'https://api-partner.network.xero.com',
-            'base_url' => 'https://api-partner.network.xero.com',
+            'site'     => 'https://api.xero.com',
+            'base_url' => 'https://api.xero.com',
         ]
     ];
 }
