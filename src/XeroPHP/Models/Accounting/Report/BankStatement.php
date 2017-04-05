8a7cb79824ce57bfa9c86e1ec8df3ad79a5b4810
Dan Litman <dan@b3it.co>
2017-04-05 12:12:57 +0100

    Adds BankStatement Report model

---
+++ src/XeroPHP/Models/Accounting/Report/BankStatement.php
@@ -0,0 +1,17 @@
+<?php
+namespace XeroPHP\Models\Accounting\Report;
+
+class BankStatement extends Report
+{
+    /**
+     * Get the resource uri of the class (Contacts) etc
+     *
+     * @return string
+     */
+    public static function getResourceURI()
+    {
+        return 'Reports/BankStatement';
+    }
+
+
+}
