8f67e351dd8d4b164c8e85a9668e577d832353a2
Michael Calcinai <michael@calcin.ai>
2017-07-27 20:22:41 +1200

    Added missing properties to ExpenseClaims

--- src/XeroPHP/Models/Accounting/ExpenseClaim.php
+++ src/XeroPHP/Models/Accounting/ExpenseClaim.php
@@ -102,9 +102,19 @@ class ExpenseClaim extends Remote\Object
         return [
             'ExpenseClaimID' => [false, self::PROPERTY_TYPE_STRING, null, false, false],
             'User' => [true, self::PROPERTY_TYPE_OBJECT, 'Accounting\\User', false, false],
-            'Receipts' => [true, self::PROPERTY_TYPE_OBJECT, 'Accounting\\Receipt', true, false]
+            'Receipts' => [true, self::PROPERTY_TYPE_OBJECT, 'Accounting\\Receipt', true, false],
+            'Payments' => [true, self::PROPERTY_TYPE_OBJECT, 'Accounting\\Payment', true, false],
+            'Status' => [false, self::PROPERTY_TYPE_ENUM, null, false, false],
+            'UpdatedDateUTC' => [false, self::PROPERTY_TYPE_TIMESTAMP, '\\DateTimeInterface', false, false],
+            'Total' => [false, self::PROPERTY_TYPE_FLOAT, null, false, false],
+            'AmountDue' => [false, self::PROPERTY_TYPE_FLOAT, null, false, false],
+            'AmountPaid' => [false, self::PROPERTY_TYPE_FLOAT, null, false, false],
+            'PaymentDueDate' => [false, self::PROPERTY_TYPE_TIMESTAMP, '\\DateTimeInterface', false, false],
+            'ReportingDate' => [false, self::PROPERTY_TYPE_TIMESTAMP, '\\DateTimeInterface', false, false],
+
         ];
     }
+
 
     public static function isPageable()
     {
@@ -172,5 +182,67 @@ class ExpenseClaim extends Remote\Object
         return $this;
     }
 
+    /**
+     * @return Payment[]
+     */
+    public function getPayments()
+    {
+        return $this->_data['Payments'];
+    }
 
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->_data['Status'];
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getUpdatedDateUTC()
+    {
+        return $this->_data['UpdatedDateUTC'];
+    }
+
+    /**
+     * @return float
+     */
+    public function getTotal()
+    {
+        return $this->_data['Total'];
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmountDue()
+    {
+        return $this->_data['AmountDue'];
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmountPaid()
+    {
+        return $this->_data['AmountPaid'];
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getPaymentDueDate()
+    {
+        return $this->_data['PaymentDueDate'];
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getReportingDate()
+    {
+        return $this->_data['ReportingDate'];
+    }
 }
