e1a91942266af121358165dd14b13f9b09671e69
Tim MacDonald <tim.mac7@me.com>
2017-08-05 21:47:32 +1000

    refactor exceptions

--- src/XeroPHP/Remote/Exception/NotAvailableException.php
+++ src/XeroPHP/Remote/Exception/NotAvailableException.php
@@ -7,14 +7,7 @@ use XeroPHP\Remote\Response;
 
 class NotAvailableException extends Exception
 {
-    public function __construct($message = null, $code = null, $previous = null)
-    {
-        if ($message === null) {
-            $message = 'API is currently unavailable.';
-        }
-        if ($code === null) {
-            $code = Response::STATUS_NOT_AVAILABLE;
-        }
-        parent::__construct($message, $code, $previous);
-    }
+    protected $message = 'API is currently unavailable.';
+
+    protected $code = Response::STATUS_NOT_AVAILABLE;
 }
