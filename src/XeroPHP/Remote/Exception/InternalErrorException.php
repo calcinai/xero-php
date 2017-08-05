491fb0587c83d6c014f7a5f8f3cfc1a1ae023b68
Tim MacDonald <tim.mac7@me.com>
2017-08-05 21:47:32 +1000

    refactor exceptions

--- src/XeroPHP/Remote/Exception/InternalErrorException.php
+++ src/XeroPHP/Remote/Exception/InternalErrorException.php
@@ -7,14 +7,7 @@ use XeroPHP\Remote\Response;
 
 class InternalErrorException extends Exception
 {
-    public function __construct($message = null, $code = null, $previous = null)
-    {
-        if ($message === null) {
-            $message = 'An unhandled error with the Xero API. Contact the Xero API team if problems persist.';
-        }
-        if ($code === null) {
-            $code = Response::STATUS_INTERNAL_ERROR;
-        }
-        parent::__construct($message, $code, $previous);
-    }
+    protected $message = 'An unhandled error with the Xero API. Contact the Xero API team if problems persist.';
+
+    protected $code = Response::STATUS_INTERNAL_ERROR;
 }
