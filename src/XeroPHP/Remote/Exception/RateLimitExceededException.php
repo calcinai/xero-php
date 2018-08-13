1ef4b1a050fe9aa03e043c3cdcff091b19bc3fb9
Josh Grant <josh@synergitech.co.uk>
2018-08-13 15:39:48 +0100

    expose x-rate-limit-problem

--- src/XeroPHP/Remote/Exception/RateLimitExceededException.php
+++ src/XeroPHP/Remote/Exception/RateLimitExceededException.php
@@ -10,4 +10,16 @@ class RateLimitExceededException extends Exception
     protected $message = 'The API rate limit for your organisation/application pairing has been exceeded.';
 
     protected $code = Response::STATUS_RATE_LIMIT_EXCEEDED;
+
+    protected $rateLimitProblem = null;
+
+    public function setRateLimitProblem($rateLimitProblem)
+    {
+        $this->rateLimitProblem = strtolower($rateLimitProblem);
+    }
+
+    public function getRateLimitProblem()
+    {
+        return $this->rateLimitProblem;
+    }
 }
