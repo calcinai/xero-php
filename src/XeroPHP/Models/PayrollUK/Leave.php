8fd2b3b17e075e339f3cd5125545315bd4fc5c4a
Paul Wright <pjw345@hotmail.com>
2022-11-07 09:13:17 +0000

    add PayrollUK/Leave class

---
+++ src/XeroPHP/Models/PayrollUK/Leave.php
@@ -0,0 +1,257 @@
+<?php
+
+namespace XeroPHP\Models\PayrollUK;
+
+use XeroPHP\Remote;
+use XeroPHP\Models\PayrollUK\Leave\Period;
+
+class Leave extends Remote\Model
+{
+    /**
+     * Xero identifier.
+     *
+     * @property string LeaveID
+     */
+
+    /**
+     * The Xero identifier for Leave Type.
+     *
+     * @property string LeaveTypeID
+     */
+
+    /**
+     * Start date of the leave (YYYY-MM-DD).
+     *
+     * @property \DateTimeInterface StartDate
+     */
+
+    /**
+     * End date of the leave (YYYY-MM-DD).
+     *
+     * @property \DateTimeInterface EndDate
+     */
+
+    /**
+     * The Description of the Leave (max length = 200).
+     *
+     * @property string Description
+     */
+
+    /**
+     * The leave period information.
+     *
+     * @property Period[] Periods
+     */
+    const LEAVE_PERIOD_STATUS_SCHEDULED = 'Scheduled';
+
+    const LEAVE_PERIOD_STATUS_PROCESSED = 'Processed';
+
+    /**
+     * Get the resource uri of the class (Contacts) etc.
+     *
+     * @return string
+     */
+    public static function getResourceURI()
+    {
+        return 'Leave';
+    }
+
+    /**
+     * Get the root node name.  Just the unqualified classname.
+     *
+     * @return string
+     */
+    public static function getRootNodeName()
+    {
+        return 'Leave';
+    }
+
+    /**
+     * Get the guid property.
+     *
+     * @return string
+     */
+    public static function getGUIDProperty()
+    {
+        return 'LeaveID';
+    }
+
+    /**
+     * Get the stem of the API (core.xro) etc.
+     *
+     * @return string
+     */
+    public static function getAPIStem()
+    {
+        return Remote\URL::API_PAYROLL;
+    }
+
+    /**
+     * Get the supported methods.
+     */
+    public static function getSupportedMethods()
+    {
+        return [
+            Remote\Request::METHOD_GET,
+            Remote\Request::METHOD_POST,
+            Remote\Request::METHOD_PUT,
+            Remote\Request::METHOD_DELETE
+        ];
+    }
+
+    /**
+     * Get the properties of the object.  Indexed by constants
+     *  [0] - Mandatory
+     *  [1] - Type
+     *  [2] - PHP type
+     *  [3] - Is an Array
+     *  [4] - Saves directly.
+     *
+     * @return array
+     */
+    public static function getProperties()
+    {
+        return [
+            'LeaveID' => [false, self::PROPERTY_TYPE_STRING, null, false, false],
+            'LeaveTypeID' => [true, self::PROPERTY_TYPE_STRING, null, false, false],
+            'Description' => [false, self::PROPERTY_TYPE_STRING, null, false, false],
+            'StartDate' => [true, self::PROPERTY_TYPE_DATE, '\\DateTimeInterface', false, false],
+            'EndDate' => [true, self::PROPERTY_TYPE_DATE, '\\DateTimeInterface', false, false],
+            'Periods' => [false, self::PROPERTY_TYPE_OBJECT, 'PayrollUK\\Leave\\Period', true, false],
+        ];
+    }
+
+    public static function isPageable()
+    {
+        return true;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLeaveID()
+    {
+        return $this->_data['LeaveID'];
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return Leave
+     */
+    public function setLeaveID($value)
+    {
+        $this->propertyUpdated('LeaveID', $value);
+        $this->_data['LeaveID'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLeaveTypeID()
+    {
+        return $this->_data['LeaveTypeID'];
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return Leave
+     */
+    public function setLeaveTypeID($value)
+    {
+        $this->propertyUpdated('LeaveTypeID', $value);
+        $this->_data['LeaveTypeID'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getStartDate()
+    {
+        return $this->_data['StartDate'];
+    }
+
+    /**
+     * @param \DateTimeInterface $value
+     *
+     * @return Leave
+     */
+    public function setStartDate(\DateTimeInterface $value)
+    {
+        $this->propertyUpdated('StartDate', $value);
+        $this->_data['StartDate'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getEndDate()
+    {
+        return $this->_data['EndDate'];
+    }
+
+    /**
+     * @param \DateTimeInterface $value
+     *
+     * @return Leave
+     */
+    public function setEndDate(\DateTimeInterface $value)
+    {
+        $this->propertyUpdated('EndDate', $value);
+        $this->_data['EndDate'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->_data['Description'];
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return Leave
+     */
+    public function setDescription($value)
+    {
+        $this->propertyUpdated('Description', $value);
+        $this->_data['Description'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return Period[]|Remote\Collection
+     */
+    public function getPeriods()
+    {
+        return $this->_data['Periods'];
+    }
+
+    /**
+     * @param Period $value
+     *
+     * @return Leave
+     */
+    public function addPeriod(Period $value)
+    {
+        $this->propertyUpdated('Periods', $value);
+        if (! isset($this->_data['Periods'])) {
+            $this->_data['Periods'] = new Remote\Collection();
+        }
+        $this->_data['Periods'][] = $value;
+
+        return $this;
+    }
+}
---
+++ src/XeroPHP/Models/PayrollUK/Leave/Period.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace XeroPHP\Models\PayrollUK\Leave;
+
+use XeroPHP\Remote;
+
+class Period extends Remote\Model
+{
+    /**
+     * The Number of Units for the leave.
+     *
+     * @property string NumberOfUnits
+     */
+
+    /**
+     * The Period End Date (YYYY-MM-DD).
+     *
+     * @property \DateTimeInterface PeriodEndDate
+     */
+
+    /**
+     * The Period Start Date (YYYY-MM-DD).
+     *
+     * @property \DateTimeInterface PeriodStartDate
+     */
+
+    /**
+     * See PeriodStatus.
+     *
+     * @property string PeriodStatus
+     */
+
+    /**
+     * Get the resource uri of the class (Contacts) etc.
+     *
+     * @return string
+     */
+    public static function getResourceURI()
+    {
+        return 'Period';
+    }
+
+    /**
+     * Get the root node name.  Just the unqualified classname.
+     *
+     * @return string
+     */
+    public static function getRootNodeName()
+    {
+        return 'Period';
+    }
+
+    /**
+     * Get the guid property.
+     *
+     * @return string
+     */
+    public static function getGUIDProperty()
+    {
+        return '';
+    }
+
+    /**
+     * Get the stem of the API (core.xro) etc.
+     *
+     * @return string
+     */
+    public static function getAPIStem()
+    {
+        return Remote\URL::API_PAYROLL;
+    }
+
+    /**
+     * Get the supported methods.
+     */
+    public static function getSupportedMethods()
+    {
+        return [
+        ];
+    }
+
+    /**
+     * Get the properties of the object.  Indexed by constants
+     *  [0] - Mandatory
+     *  [1] - Type
+     *  [2] - PHP type
+     *  [3] - Is an Array
+     *  [4] - Saves directly.
+     *
+     * @return array
+     */
+    public static function getProperties()
+    {
+        return [
+            'NumberOfUnits' => [false, self::PROPERTY_TYPE_STRING, null, false, false],
+            'PeriodEndDate' => [false, self::PROPERTY_TYPE_DATE, '\\DateTimeInterface', false, false],
+            'PeriodStartDate' => [false, self::PROPERTY_TYPE_DATE, '\\DateTimeInterface', false, false],
+            'PeriodStatus' => [false, self::PROPERTY_TYPE_ENUM, null, false, false],
+        ];
+    }
+
+    public static function isPageable()
+    {
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumberOfUnits()
+    {
+        return $this->_data['NumberOfUnits'];
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return Period
+     */
+    public function setNumberOfUnit($value)
+    {
+        $this->propertyUpdated('NumberOfUnits', $value);
+        $this->_data['NumberOfUnits'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getPeriodEndDate()
+    {
+        return $this->_data['PeriodEndDate'];
+    }
+
+    /**
+     * @param \DateTimeInterface $value
+     *
+     * @return Period
+     */
+    public function setPeriodEndDate(\DateTimeInterface $value)
+    {
+        $this->propertyUpdated('PeriodEndDate', $value);
+        $this->_data['PeriodEndDate'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getPeriodStartDate()
+    {
+        return $this->_data['PeriodStartDate'];
+    }
+
+    /**
+     * @param \DateTimeInterface $value
+     *
+     * @return Period
+     */
+    public function setPeriodStartDate(\DateTimeInterface $value)
+    {
+        $this->propertyUpdated('PeriodStartDate', $value);
+        $this->_data['PeriodStartDate'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPeriodStatus()
+    {
+        return $this->_data['PeriodStatus'];
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return Period
+     */
+    public function setPeriodStatus($value)
+    {
+        $this->propertyUpdated('PeriodStatus', $value);
+        $this->_data['PeriodStatus'] = $value;
+
+        return $this;
+    }
+}
